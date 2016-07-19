1d1ef5b698dcbf0147f71d4908dc9541b8324497
sakshamu <sakshamu@users.noreply.github.com>
2016-07-18 20:42:07 -0400

    Shifted Supplementary section after Core, added favicon, updated link in footer

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.engine('.hbs', exphbs({
 app.set('view engine', '.hbs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
